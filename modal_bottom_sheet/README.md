a797d92e32e24baa9124047aafd610c896220494
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-03-02 23:58:40 -0300

    initial commit

---
+++ modal_bottom_sheet/README.md
@@ -0,0 +1,5 @@
+# Modal Bottom Sheet In Flutter
+
+Uma alternativa a um menu ou caixa de diálogo, evitando que o usuário interaja com o resto do aplicativo.
+
+Link para artigo [Flutterdevs.](https://medium.com/flutterdevs/modal-bottom-sheet-in-flutter-dae05debbed2)
---
+++ modal_bottom_sheet/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ModalBottomSheet(),
+    );
+  }
+}
