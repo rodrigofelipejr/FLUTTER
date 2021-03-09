4c4389f25ddaa9abc41dbf7bdada91a28f5565b0
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-03-09 13:31:56 -0300

    O Domain

---
+++ github_search/lib/modules/search/domain/entities/result_search.dart
@@ -0,0 +1,9 @@
+class ResultSearch {
+  final String image;
+  final String title;
+  final String content;
+
+  ResultSearch({this.image, this.title, this.content});
+}
+
+// Nossa entidade deve ser o mais pura possível, sem o maps por exemplo...
