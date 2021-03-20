b6dd6460821a008b068894d4007eabfa79a613c2
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-03-20 16:28:25 -0300

    aula 04 - corrigir stream

---
+++ aula_04/lib/home/repositories/home_repository_impl.dart
@@ -0,0 +1,47 @@
+import 'package:aula_04/shared/constants.dart';
+import 'package:hasura_connect/hasura_connect.dart';
+
+import 'home_repository.dart';
+
+class HomeRepositoryImpl extends HomeRepository {
+  final _client = HasuraConnect(HASURA_URL);
+
+  @override
+  Future<List<Map>> getTarefas() async {
+    final response = await _client.query('''
+      query MyQuery {
+        posts {
+          id
+          name
+        }
+      }    
+    ''');
+
+    return (response['data']['posts'] as List).map((e) => {'name': e['name']}).toList();
+  }
+
+  @override
+  Stream streamTarefas() {
+    // TODO: implement streamTarefas
+    throw UnimplementedError();
+  }
+
+  streamTarefas2() async {
+    String docSubscription = '''
+    subscription{
+      posts {
+        id
+        name
+        photo
+      }
+    }
+    ''';
+
+    Snapshot snapshot = await _client.subscription(docSubscription);
+    snapshot.listen((data) {
+      print(data);
+    }).onError((err) {
+      print(err);
+    });
+  }
+}
---
+++ aula_04/lib/shared/constants.dart
@@ -0,0 +1,1 @@
+const String HASURA_URL = "https://hasura-aula-04.hasura.app/v1/graphql";
